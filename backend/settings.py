9515f19ccba7fbf72668fe6374ed779e63781f36
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2025-01-05 09:44:21 +0100

    backend fixes all1

--- backend/settings.py
+++ backend/settings.py
@@ -53,7 +53,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173/',
+]
+
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
